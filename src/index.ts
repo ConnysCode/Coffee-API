72e17de5f5c6206c45078d9f7c15bfd4dc18871c
ConnysCode <concode@outlook.de>
2023-12-19 10:21:17 +0100

    feat(cors): add cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Elysia } from "elysia";
 import iced from "./sources/iced";
 import hot from "./sources/hot";
+import { cors } from "@elysiajs/cors";
 
 const app = new Elysia()
+  .use(cors({ methods: ["GET"], origin: "*" }))
   .get("/iced", () => iced)
   .get("/hot", () => hot)
   .listen(3000);
